2784eea9bda55c08b57562f5b3f3f66bead222d1
nagon86 <27137487+nagon86@users.noreply.github.com>
2025-03-05 20:53:23 +0200

    Try to force CORS

--- backend/server.js
+++ backend/server.js
@@ -42,6 +42,17 @@ const corsOptions = {
   },
   credentials: true,
 };
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+  next();
+});
+
 app.use(cors());
 app.use(bodyParser.json());
 
