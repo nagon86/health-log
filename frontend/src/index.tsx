303eee60c7eb980cb97f20da622f766cd8cc46b8
nagon86 <27137487+nagon86@users.noreply.github.com>
2025-03-18 21:45:33 +0200

    Enable dark theme

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,19 +1,29 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import CssBaseline from '@mui/material/CssBaseline';
+import CssBaseline from "@mui/material/CssBaseline";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
 
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
 
 // Render the React application
-const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+  },
+});
 
 root.render(
-    <React.StrictMode>
-        <CssBaseline enableColorScheme />
-        <App />
-    </React.StrictMode>
+  <React.StrictMode>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline enableColorScheme />
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
 );
